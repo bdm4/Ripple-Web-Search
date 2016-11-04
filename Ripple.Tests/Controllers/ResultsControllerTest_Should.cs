ef354c85195eb850736e7477a80ea6d0e8c04e10
Ben Margevicius <bdm4@case.edu>
2016-11-04 12:53:58 -0400

    Stubbing out search results tests

---
+++ Ripple.Tests/Controllers/ResultsControllerTest_Should.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ripple.Tests.Controllers
+{
+    [TestClass]
+    public class ResultsControllerTest_Should
+    {
+        [TestMethod]
+        public void ReturnKeywordResults()
+        {
+        }
+
+        [TestMethod]
+        public void ReturnCategoryResults()
+        {
+        }
+
+        [TestMethod]
+        public void ReturnDatesResults()
+        {
+        }
+
+        [TestMethod]
+        public void ReturnCityResults()
+        {
+        }
+
+        [TestMethod]
+        public void ReturnVenueResults()
+        {
+        }
+    }
+}
