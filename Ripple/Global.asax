9e8401ed425f23a3277cd3918c6434d6b7f563ea
Ben Margevicius <bdm4@case.edu>
2016-11-04 12:34:40 -0400

    Add project files.

---
+++ Ripple/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ripple.MvcApplication" Language="C#" %>
---
+++ Ripple/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ripple.Startup))]
+namespace Ripple
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
